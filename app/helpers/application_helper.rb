3c8e4d8545c001ad1d51b15ab65f80a5219d1d60
yeinercq <ycarvajalq@gmail.com>
2019-02-22 09:54:55 -0500

    Add a full title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+	#Returns teh full title on a per-page basis
+	def full_title(page_title = '')
+		base_title = "Taste Project"
+		if page_title.empty?
+			base_title
+		else
+			page_title + " | " + base_title
+		end
+	end
 end
