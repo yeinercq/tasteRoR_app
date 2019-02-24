0fa75ad37839ae56e03947ce59564cc32a748634
yeinercq <ycarvajalq@gmail.com>
2019-02-24 16:26:21 -0500

    Use SSL and the Puma webserver in production

--- config/puma.rb
+++ config/puma.rb
@@ -32,7 +32,7 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 # process behavior so workers use less memory.
 #
 
-on_workers_boot do
+on_worker_boot do
 	# Worker specific setup for Rails 4.1+
   # See: https://devcenter.heroku.com/articles/
   # deploying-rails-applications-with-the-puma-web-server#on-worker-boot
