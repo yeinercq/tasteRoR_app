41016617fbb544aca8a28d86871f925db788a079
yeinercq <ycarvajalq@gmail.com>
2019-04-15 22:35:14 -0500

    Add account activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,12 @@ class UsersController < ApplicationController
   before_action :admin_user,      only: :destroy
 
   def index
-    @users = User.paginate(page: params[:page])
+    @users = User.where(activated: true).paginate(page: params[:page])
   end
 
   def show
   	@user = User.find(params[:id])
+    redirect_to root_url and return unless @user.activated?
   	#debugger
   end
 
@@ -19,9 +20,9 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
   	if @user.save
-      log_in(@user)
-  		flash[:success] = "Welcome to the Taste Project!"
-  		redirect_to user_url(@user) #it's same 'redirect_to @user'
+      @user.send_activation_email
+      flash[:info] = "Please check your email to active your account"
+  		redirect_to root_url #it's same 'redirect_to @user'
   	else
   		render 'new'
   	end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create!(name: 					"Example User",
-						 email: 				"user@example.com",
-						 password: 				"passwd",
+User.create!(name: 									"Example User",
+						 email: 								"user@example.com",
+						 password: 							"passwd",
 			 			 password_confirmation: "passwd",
-			 			 admin: true)
+			 			 admin: true,
+			 			 activated: true,
+			 			 activated_at: Time.zone.now)
 
 99.times do |n|
 	name = Faker::Name.name
@@ -18,5 +20,7 @@ User.create!(name: 					"Example User",
 	User.create!(name: 									name,
 							 email: 								email,
 							 password: 							password,
-							 password_confirmation: password)
+							 password_confirmation: password,
+							 activated: true,
+							 activated_at: Time.zone.now)
 end
