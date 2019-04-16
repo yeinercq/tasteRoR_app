feee1ccdc6c54d6f58767df39ab59d47e61a4209
yeinercq <ycarvajalq@gmail.com>
2019-04-16 13:30:53 -0500

    SendGrid fix 3

--- app/models/user.rb
+++ app/models/user.rb
@@ -47,7 +47,7 @@ class User < ApplicationRecord
 
 	# Sends activation email
 	def send_activation_email
-		UserMailer.account_activation(self).deliver_now
+		UserMailer.account_activation(self).deliver
 	end
 
 	private
